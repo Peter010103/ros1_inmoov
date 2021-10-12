357e72c86b36932a56413501a74f963d29180b6f
htb27 <htb27@cam.ac.uk>
2021-10-12 18:53:11 +0100

    Removed some superfluous code.

--- Angle_Calibration/calibrateAngles.py
+++ Angle_Calibration/calibrateAngles.py
@@ -1,3 +1,4 @@
+#O to accept the angle
 Debug = True
 
 import yaml
@@ -5,11 +6,6 @@ import msvcrt as m
 if not Debug:
     from adafruit_servokit import ServoKit
     kit = ServoKit(channels=48)
-
-# W to increase the angle, S to decrease the angle, O to accept the angle
-joints = ['r_thumb', 'r_index', 'r_middle', 'r_ringFinger', 'r_pinky', 'r_wrist', 'r_bicep', 'r_arm_rotate', 'r_shoulder', 'r_omoplate',
-          'l_thumb', 'l_index', 'l_middle', 'l_ringFinger', 'l_pinky', 'l_wrist', 'l_bicep', 'l_arm_rotate', 'l_shoulder', 'l_omoplate',
-          'neck', 'rothead', 'rollneck', 'eyelid', 'jaw', 'eyeX', 'eyeY', 'topstom', 'midstom', 'lowstom']
 
 servoNamesToIndices = {}
 with open(r'jointNamesToServoNumber.yml') as inputFile:
@@ -25,7 +21,7 @@ def moveToAngle(joint, angle):
         kit.servo[servoNamesToIndices[joint]].angle = angle
 
 
-for joint in joints:
+for joint in servoNamesToIndices:
     currentAngle = 0
     skipJoint = False
     # First set the closed position of the joint
