d89742929d87e663be53f8b73c86bec7322786ec
Henry Batchelor <htb27@cam.ac.uk>
2022-02-22 15:59:52 +0000

    Added in callbacks and sending of data to a topic for the servo angles.

--- src/joint_limiter/joint_limiter.py
+++ src/joint_limiter/joint_limiter.py
@@ -66,35 +66,35 @@ def setupAngleArray():
     with open("data.yaml", 'r') as stream:
         joint_dict = yaml.safe_load(stream)
     for i, val in enumerate(['R_WRIST',
-              'R_THUMB',
-              'R_INDEX',
-              'R_MIDDLE',
-              'R_RINGFINGER',
-              'R_PINKY',
-              'R_BICEP',
-              'R_ARM_ROTATE',
-              'R_SHOULDER',
-              'R_OMOPLATE',
-              'L_THUMB',
-              'L_INDEX',
-              'L_MIDDLE',
-              'L_RINGFINGER',
-              'L_PINKY',
-              'L_WRIST',
-              'L_BICEP',
-              'L_ARM_ROTATE',
-              'L_SHOULDER',
-              'L_OMOPLATE',
-              'NECK',
-              'ROTHEAD',
-              'ROLLNECK',
-              'EYELID',
-              'JAW',
-              'EYEX',
-              'EYEY',
-              'TOPSTOM',
-              'MIDSTOM',
-              'LOWSTOM']):
+                             'R_THUMB',
+                             'R_INDEX',
+                             'R_MIDDLE',
+                             'R_RINGFINGER',
+                             'R_PINKY',
+                             'R_BICEP',
+                             'R_ARM_ROTATE',
+                             'R_SHOULDER',
+                             'R_OMOPLATE',
+                             'L_THUMB',
+                             'L_INDEX',
+                             'L_MIDDLE',
+                             'L_RINGFINGER',
+                             'L_PINKY',
+                             'L_WRIST',
+                             'L_BICEP',
+                             'L_ARM_ROTATE',
+                             'L_SHOULDER',
+                             'L_OMOPLATE',
+                             'NECK',
+                             'ROTHEAD',
+                             'ROLLNECK',
+                             'EYELID',
+                             'JAW',
+                             'EYEX',
+                             'EYEY',
+                             'TOPSTOM',
+                             'MIDSTOM',
+                             'LOWSTOM']):
         joints_values[i, 0] = joint_dict[val]['closed']
         joints_values[i, 1] = joint_dict[val]['rest']
         joints_values[i, 2] = joint_dict[val]['open']
@@ -107,7 +107,7 @@ def average(li):
         output_array[v] = temp
 
 
-def leftArmCallBack(data):
+def leftArmCallback(data):
     joints_list[Joints.L_BICEP, iterator[JointTopic.L_ARM]] = data.data[0]
     joints_list[Joints.L_ARM_ROTATE, iterator[JointTopic.L_ARM]] = data.data[1]
     joints_list[Joints.L_SHOULDER, iterator[JointTopic.L_ARM]] = data.data[2]
@@ -117,7 +117,7 @@ def leftArmCallBack(data):
             Joints.L_SHOULDER, Joints.L_OMOPLATE])
 
 
-def rightArmCallBack(data):
+def rightArmCallback(data):
     joints_list[Joints.R_BICEP, iterator[JointTopic.R_ARM]] = data.data[0]
     joints_list[Joints.R_ARM_ROTATE, iterator[JointTopic.R_ARM]] = data.data[1]
     joints_list[Joints.R_SHOULDER, iterator[JointTopic.R_ARM]] = data.data[2]
@@ -126,15 +126,56 @@ def rightArmCallBack(data):
     average([Joints.R_BICEP, Joints.R_ARM_ROTATE,
             Joints.R_SHOULDER, Joints.R_OMOPLATE])
 
-# to work on
+def spinalCallback(data):
+    joints_list[Joints.NECK, iterator[JointTopic.SPINAL_COLUMN]] = data.data[0]
+    joints_list[Joints.ROTHEAD,
+                iterator[JointTopic.SPINAL_COLUMN]] = data.data[1]
+    joints_list[Joints.ROLLNECK,
+                iterator[JointTopic.SPINAL_COLUMN]] = data.data[2]
+    iterator[JointTopic.SPINAL_COLUMN] = (
+        iterator[JointTopic.SPINAL_COLUMN] + 1) % 8
+    average([Joints.NECK,
+            Joints.ROTHEAD, Joints.ROLLNECK])
 
+
+def leftHandCallback(data):
+    joints_list[Joints.L_THUMB, iterator[JointTopic.L_HAND]] = data.data[0]
+    joints_list[Joints.L_INDEX, iterator[JointTopic.L_HAND]] = data.data[1]
+    joints_list[Joints.L_MIDDLE, iterator[JointTopic.L_HAND]] = data.data[2]
+    joints_list[Joints.L_RINGFINGER,
+                iterator[JointTopic.L_HAND]] = data.data[3]
+    joints_list[Joints.L_PINKY,
+                iterator[JointTopic.L_HAND]] = data.data[4]
+    iterator[JointTopic.L_HAND] = (iterator[JointTopic.L_HAND] + 1) % 8
+    average([Joints.L_THUMB, Joints.L_INDEX, Joints.L_MIDDLE,
+            Joints.L_RINGFINGER, Joints.L_PINKY])
+
+
+def rightHandCallback(data):
+    joints_list[Joints.R_THUMB, iterator[JointTopic.R_HAND]] = data.data[0]
+    joints_list[Joints.R_INDEX, iterator[JointTopic.R_HAND]] = data.data[1]
+    joints_list[Joints.R_MIDDRE, iterator[JointTopic.R_HAND]] = data.data[2]
+    joints_list[Joints.R_RINGFINGER,
+                iterator[JointTopic.R_HAND]] = data.data[3]
+    joints_list[Joints.R_PINKY,
+                iterator[JointTopic.R_HAND]] = data.data[4]
+    iterator[JointTopic.R_HAND] = (iterator[JointTopic.R_HAND] + 1) % 8
+    average([Joints.R_THUMB, Joints.R_INDEX, Joints.R_MIDDLE,
+            Joints.R_RINGFINGER, Joints.R_PINKY])
+
+def faceCallback(data):
+    return
 
 def clipOutputArray():
-    output_array = np.clip(output_array, joints_values[:, 0], joints_values[:, 2])
+    output_array = np.clip(
+        output_array, joints_values[:, 0], joints_values[:, 2])
 
 
 def listener():
     rospy.init_node('/joint_limiter', anonymous=True)
+    joint_pub = rospy.Publisher(
+        '/servo_angles', Int16MultiArray, queue_size=10)
+    rate = rospy.Rate(10)  # Run at 10Hz
     joint_sub0 = rospy.Subscriber(
         '/joints/spinal_column', Int16MultiArray, spinalCallback)
     joint_sub1 = rospy.Subscriber(
@@ -147,10 +188,11 @@ def listener():
         '/joints/hand/right', Int16MultiArray, rightHandCallback)
     joint_sub5 = rospy.Subscriber(
         '/joints/face', Int16MultiArray, faceCallback)
-    try:
-        rospy.spin()
-    except KeyboardInterrupt:
-        print("shutting down")
+    while True:
+        data_to_send = Int16MultiArray()
+        data_to_send.data = output_array
+        joint_pub.publish(data_to_send)
+        rate.sleep()
 
 
 if __name__ == "__main__":
